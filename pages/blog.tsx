97d5bb6b0f5e7ac979972f349a1ef9ad2bb48ce7
Rita <margvlahou@gmail.com>
2022-04-09 18:41:18 +0100

    change title

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -6,7 +6,7 @@ const Blog: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Personal stylist</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
